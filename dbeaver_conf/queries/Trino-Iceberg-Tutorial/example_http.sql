2cb3f912cc7d93e5ac3e98a8b68951d0e05ffce2
Egor Tarasenko <egor@ponder.co>
2024-11-14 14:43:19 +0200

    FIX: Data Files

--- dbeaver_conf/queries/Trino-Iceberg-Tutorial/example_http.sql
+++ dbeaver_conf/queries/Trino-Iceberg-Tutorial/example_http.sql
@@ -1,3 +1,5 @@
 -- https://github.com/trinodb/trino/tree/master/plugin/trino-example-http/src/test/resources/example-data
 
 select * from http.example.numbers;
+
+select * from local_http.example.numbers;
