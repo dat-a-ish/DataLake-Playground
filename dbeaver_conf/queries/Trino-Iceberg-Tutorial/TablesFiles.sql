cc8cba19ac050268990826f6445a0b4c89bc50b5
Egor Tarasenko <egor@ponder.co>
2024-11-14 10:49:11 +0200

    FEAT: Add tables files

---
+++ dbeaver_conf/queries/Trino-Iceberg-Tutorial/TablesFiles.sql
@@ -0,0 +1,5 @@
+select * from iceberg_datalake.test_schema."test_table$files";
+
+select * from iceberg_datalake.test_schema."test_table_partitioned$files";
+
+select * from iceberg_datalake.dbt."test_table_postgres$files";
